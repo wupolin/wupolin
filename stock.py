246761c69c64653a67e1d0018fa52e4c3197838f
wupolin <wupolin0102@gmail.com>
2018-07-17 13:54:01 +0800

    modified:   stock.py

--- stock.py
+++ stock.py
@@ -21,10 +21,10 @@ def parse_article_meta(entry): #step3
 
 def print_stockinfo(day,post_entries):
     if len(post_entries)==0:
-        print('There isn\'t existing this stock')
+        print('There isn\'t existing this stock symbol')
     for entry in post_entries: 
         meta = parse_article_meta(entry)
-        print('This is '+meta['stocknum'][0].text[8:16]) #print STOCK NO. and name 
+        print(meta['stocknum'][0].text[8:16]) #print STOCK NO. and name 
         datatype=(meta['datatype'][0].text,meta['datatype'][3].text,meta['datatype'][4].text,meta['datatype'][5].text,meta['datatype'][6].text)
         datalist=[]
         i=0
@@ -41,7 +41,7 @@ def print_stockinfo(day,post_entries):
         i=0
         while i<day:  #make the two row's data combine
             if i==10:
-                print('The system is just storing the information of the stock in 10 days,\nso it\'s just shown the information in 10 days here')
+                print('The system is just storing the information of the stock in 10 days,\nso it\'s just shown the information in 10 days')
                 break
             print(meta['datatype'][0].text+' : '+datalist[i][0])
             print(meta['datatype'][1].text+' : '+datalist[i][1])
@@ -50,13 +50,17 @@ def print_stockinfo(day,post_entries):
             print(meta['datatype'][5].text+' : '+datalist[i][5])
             i=i+1
 while 1:
-    stocknum=input('please input the number of the stock :')
+    stocknum=input('please input the stock symbol (e.g. 2330) :')
+    while stocknum.isdigit() == False or len(stocknum)>4:
+        stocknum=input('Sorry, please input the correct type of stock symbol (e.g. 2330) :')
     url = 'http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date=20180715&stockNo='+stocknum
-    day=input('please input the number of day :')
+    day=input('please input the number of output days (e.g. 5) :')
+    while day.isdigit() == False:
+        day=input('Sorry, please input the type of Integer (e.g. 5) :')
     resp = fetch(url)  # step-1
     post_entries = parse_article_entries(resp.text)  # step-2
     print_stockinfo(int(day),post_entries)
-    check=input('Do you want to get another information of stock? Y/n: ')
+    check=input('Do you want to get the information of another stock? Y/n: ')
     if(check!='Y' and check!='y'):
         break
 
